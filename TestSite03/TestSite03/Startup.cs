0a799600fa62dceba9650f107d642b0948a3536a
Nigel Hope <n.hope@wordplay.media>
2017-02-13 17:28:23 +0000

    Default MVC Test Site

---
+++ TestSite03/TestSite03/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestSite03.Startup))]
+namespace TestSite03
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
